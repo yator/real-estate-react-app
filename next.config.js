3015eeff02eda4d9e388f9e6501beab256d9a52e
yator <abelkipruto4@gmail.com>
2022-07-15 13:40:15 +0300

    ignore eslint during build

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   images:{
     domains:['bayut-production.s3.eu-central-1.amazonaws.com']
 },
+eslint: {
+  // Warning: This allows production builds to successfully complete even if
+  // your project has ESLint errors.
+  ignoreDuringBuilds: true,
+},
 }
